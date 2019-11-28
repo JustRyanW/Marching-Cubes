53d466cd9b855322b7eace2031e7db8c50122601
Ryan Walker <ryan.walker@tombola.com>
2019-11-28 14:49:01 +0000

    Working Compute Shader!

---
+++ Assets/Compute testing/shader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shader : MonoBehaviour
+{
+    public ComputeShader shader1;
+    [Space]
+    [SerializeField]
+    Vector3[] data = new Vector3[1];
+    [SerializeField]
+    Vector3[] output = new Vector3[1];
+
+
+    private void Start()
+    {
+        RunShader();
+    }
+
+    void RunShader()
+    {
+        ComputeBuffer buffer = new ComputeBuffer(data.Length, 12);
+        buffer.SetData(data);
+        int kernel = shader1.FindKernel("Multiply");
+        shader1.SetBuffer(kernel, "dataBuffer", buffer);
+        shader1.Dispatch(kernel, data.Length, 1, 1);
+        buffer.GetData(output);
+        buffer.Dispose();
+    }
+}
