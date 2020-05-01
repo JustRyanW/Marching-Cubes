73fdfd5a9a4961189cd5f5473ee2d45f3b8c2f92
Ryan <xryanjw@gmail.com>
2020-05-01 22:17:47 +0100

    Bitwise operators

--- Assets/Scripts/Voxels.cs
+++ Assets/Scripts/Voxels.cs
@@ -1,22 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 
 public class Voxels : MonoBehaviour
 {
     public Vector3Int size = new Vector3Int(8, 8, 8);
     public float surface = 0;
 
-    private float[,,] voxels = new float[9, 9, 9];
+    private float[,,] voxels = new float[8, 8, 8];
+
+    private void Start() { 
+
+        // List all offsets
+        for (int i = 0; i < 8; i++)
+        {
+            float x = (i & 4) >> 2;
+            float y = (i & 2) >> 1;
+            float z = i & 1;
+
+           // Debug.Log(x + " " + y + " " + z);
+        }
+    }
 
     [ContextMenu("Randomize Voxels")]
     private void Randomize() {
-        for (int x = 0; x < voxels.GetLength(0); x++)
+        for (int x = 0; x < size.x; x++)
         {
-            for (int y = 0; y < voxels.GetLength(1); y++)
+            for (int y = 0; y < size.y; y++)
             {
-                for (int z = 0; z < voxels.GetLength(2); z++)
+                for (int z = 0; z < size.z; z++)
                 {
                     voxels[x, y, z] = Random.Range(0f, 1f);
                 }
@@ -29,16 +41,16 @@ public class Voxels : MonoBehaviour
         Gizmos.color = Color.white;
         Gizmos.DrawWireCube((Vector3)size / 2, size);
 
-        for (int x = size.x; x >= 0; x--)
+        for (int x = size.x - 1; x >= 0; x--)
         {
-            for (int y = size.y; y >= 0; y--)
+            for (int y = 0; y < size.y; y++)
             {
-                for (int z = size.z; z >= 0; z--)
+                for (int z = size.z - 1; z >= 0; z--)
                 {
                     if (voxels[x, y, z] >= surface)
                     {
                         Gizmos.color = Color.Lerp(Color.black, Color.white, voxels[x, y, z]);
-                        Gizmos.DrawSphere(new Vector3(x, y, z), 0.1f);   
+                        Gizmos.DrawCube(new Vector3(x, y, z) + Vector3.one * 0.5f, Vector3.one * 0.1f);   
                     }
                 }
             }
@@ -46,10 +58,10 @@ public class Voxels : MonoBehaviour
     }
 
     private void OnValidate() {
-        if (size.x + 1 != voxels.GetLength(0) || size.y + 1 != voxels.GetLength(1) || size.z + 1 != voxels.GetLength(2))
+        if (size.x != voxels.GetLength(0) || size.y != voxels.GetLength(1) || size.z != voxels.GetLength(2))
         {
             size.Clamp(Vector3Int.one, Vector3Int.one * 16);
-            voxels = new float[size.x + 1, size.y + 1, size.z + 1];
+            voxels = new float[size.x, size.y, size.z];
         }
 
         surface = Mathf.Clamp01(surface);
