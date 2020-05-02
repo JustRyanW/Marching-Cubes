1fa39b64efae158c15c1c5f5e14d08b0d1cfee0a
Ryan <xryanjw@gmail.com>
2020-05-02 22:05:23 +0100

    Chunks

---
+++ Assets/Scripts/NoiseFilter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseFilter {
+    Noise noise = new Noise();
+
+    public float Evaluate(Vector3 point) {
+        float noiseValue = Mathf.Clamp01(noise.Evaluate(point * 0.015f));
+        return noiseValue;
+    }
+}
